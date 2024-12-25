5f8a14d7f47bd624031cd2c672e8184df32d3179
Wisam Maadkhana <wisamm@Wisams-MacBook-Pro.local>
2024-12-25 01:47:06 +0100

    test fix

--- simulated_pos/tests/conftest.py
+++ simulated_pos/tests/conftest.py
@@ -3,6 +3,21 @@ from app import app, db
 from models import User, MenuItem
 
 @pytest.fixture
+def application():
+    app.config['TESTING'] = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+    app.config['JWT_SECRET_KEY'] = 'test-key'
+    
+    with app.app_context():
+        db.create_all()
+        yield app
+        db.drop_all()
+
+@pytest.fixture
+def client(application):
+    return application.test_client()
+
+@pytest.fixture
 def client():
     app.config['TESTING'] = True
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
